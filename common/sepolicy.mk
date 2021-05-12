58596a00abac017d3b045a514ac6198a2844d562
Chenyang Zhong <zhongcy95@gmail.com>
2021-05-12 01:21:04 -0400

    sepolicy: move and guard access to power HAL
    On devices with prebuilt vendor, if the power HAL accesses input
    device nodes to support DT2W related feature, the resulting sepolicy
    may trigger following neverallow in system/sepolicy/public/app.te:
    
    neverallow {
      appdomain
      -shell # bugreport
    } input_device:chr_file ~getattr;
    
    Since the vendor image and the vendor sepolicy are prebuilt,
    power HAL sepolicy on such devices is often written in the following
    fashion:
    
    allow hal_power input_device:dir search;
    allow hal_power input_device:chr_file rw_file_perms;
    
    This combined with the flipendo/turbo_adapter sepolicy will give
    the app(s) direct access to input device nodes, thus triggering
    the neverallow. Considering that flipendo and turbo_adapter's
    access to power HAL is not critical, guard the sepolicy to allow
    opting out.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Change-Id: Iac02247e2b0ac5ba5f3f464bd0b21ef41b99693b
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -25,8 +25,13 @@ ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/conquer/sepolicy/common/dynamic \
     device/conquer/sepolicy/common/system
+ifneq ($(TARGET_HAL_POWER_RW_INPUT_DEVICE), true)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
+    device/conquer/sepolicy/common/dynamic_extra
+endif
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/conquer/sepolicy/common/dynamic \
+    device/conquer/sepolicy/common/dynamic_extra \
     device/conquer/sepolicy/common/vendor
 endif
